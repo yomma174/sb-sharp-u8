4aa680234d8c027126e6caf0b3f414e510adfee9
yomma <furbolg174@gmail.com>
2024-04-13 07:16:36 +0500

    ic

---
+++ sb-sharp-u8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sb_sharp_u8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
